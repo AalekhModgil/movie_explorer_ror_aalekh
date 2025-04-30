5ba065d8d3d5e7069cb7597f47a2217b2754dc46
Aalekh Modgil <aalekhmodgil8@gmail.com>
2025-04-30 19:30:26 +0530

    [Aalekh] ADD: Swagger implementation for user and movies

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -29,6 +29,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name, :mobile_number)
+    params.require(:user).permit(:email, :password, :name, :mobile_number)
   end
 end
--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,38 +1,48 @@
 FactoryBot.define do
   factory :movie do
-    title { Faker::Movie.title }
-    genre { Faker::Book.genre }
+    title { Faker::Movie.title[0...100] }
+    genre { %w[Action Comedy Drama Thriller Sci-Fi].sample }
     release_year { Faker::Number.between(from: 1881, to: Date.current.year + 1) }
     rating { Faker::Number.between(from: 0, to: 10) }
     director { Faker::Name.name }
-    duration { Faker::Number.between(from: 1, to: 300) } 
-    description { Faker::Lorem.paragraph_by_chars(number: 500, supplemental: false) } 
+    duration { Faker::Number.between(from: 60, to: 240) }
+    streaming_platform { %w[Amazon Netflix Hulu Disney+ HBO].sample }
+    main_lead { Faker::Name.name }
+    description { Faker::Lorem.paragraph(sentence_count: 5)[0...1000] }
     premium { false }
 
-    poster do
-      Rack::Test::UploadedFile.new(
-        Rails.root.join('spec', 'fixtures', 'files', 'poster.jpg'),
-        'image/jpg'
-      )
-    end
-
-    banner do
-      Rack::Test::UploadedFile.new(
-        Rails.root.join('spec', 'fixtures', 'files', 'banner.png'),
-        'image/jpg'
-      )
-    end
-
+    # Trait for premium movies
     trait :premium do
       premium { true }
     end
 
-    trait :without_poster do
-      poster { nil }
+    # Trait for invalid movie data
+    trait :invalid do
+      title { '' }
+      genre { '' }
+      release_year { 1800 } # Invalid: before 1881
+      rating { 11 } # Invalid: greater than 10
+      director { '' }
+      duration { 0 } # Invalid: must be greater than 0
+      streaming_platform { 'InvalidPlatform' }
+      main_lead { '' }
+      description { '' }
     end
 
-    trait :without_banner do
-      banner { nil }
+    # Trait to attach poster and banner
+    trait :with_attachments do
+      after(:build) do |movie|
+        movie.poster.attach(
+          io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'poster.jpg')),
+          filename: 'poster.jpg',
+          content_type: 'image/jpeg'
+        )
+        movie.banner.attach(
+          io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'banner.jpg')),
+          filename: 'banner.jpg',
+          content_type: 'image/jpeg'
+        )
+      end
     end
   end
 end
