db4afd8a09a13c595b7b9994083a11fad1e3c3b0
Aalekh Modgil <aalekhmodgil8@gmail.com>
2025-06-14 23:52:57 +0530

    [Aalekh] MOD: Fixed bugs in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-AdminUser.create!(email: 'aalekhmodgil8@gmail.com', password: 'Aalekh@1234', password_confirmation: 'password')
+AdminUser.create!(email: 'aalekhmodgil8@gmail.com', password: 'Aalekh@1234', password_confirmation: 'Aalekh@1234')
